1a03af8128389fd9409e9b7251f6dae8c751194b
Tim Toebrock <timtoebrock@googlemail.com>
2018-07-25 14:36:49 +0200

    Improved comment sceleton

--- TestData.R
+++ TestData.R
@@ -2,12 +2,9 @@ library(arules)
  
 
 ## Creata sample Datat ##
-
-
+data <- list(c('A', "B", "C"))
 ## Run aprioi on it from arules ##
 
-
 ## run you apriori on it ##
-
 
 ## make unit test whether they are the same ##
