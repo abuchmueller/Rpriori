79a0c1e8b1644ee218edbc9505eb61ffad93ca49
anbu3000 <andreas.buchmueller@icloud.com>
2018-08-09 18:39:15 +0200

    print method for rules now only prints rules with consequent length=1. added examples to examples folder folder

--- R/AllPrintMethods.R
+++ R/AllPrintMethods.R
@@ -2,55 +2,44 @@
 #############################################################################################
 
 # This tells the generic print function what to print when the object is of class ngTMatrix
-# Printing method for TransactionsMatrix class 
-# For now it prints the names of the items on the TransactionMatrix
+# Printing method for TransactionsMatrix class (later)
+# For now it prints the names of the items in the TransactionMatrix
 
 setMethod("print", signature(x = "ngTMatrix"), function(x) {
   print(x@Dimnames[[1]])
 })
-#print(TransactionMatrix)
+
 
 # Printing method for transactions class
 setMethod("print", signature(x = "transactions"), function(x) {
   print(x@itemInfo)
 })
 
-## FrequentItems
+## FrequentItems ##
 # Idea here is to combine the elements of both lists, i.e., a frequent item and it's corressponding support and display them side by side column wise in a data frame
 
 # setClass() ## Placeholder to implement FrequentItems properly as an S4 class later on
 
-# For now we create an S3 class from frequent_items and coerce it into an S4 class (Reactivate this block after building package)
-#class(frequent_items) <- "FrequentItems" 
-#setOldClass("FrequentItems") 
-#isS4(frequent_items)
-
 # Printing method for FrequentItems class
 setMethod("print", signature(x = "FrequentItems"), function(x) {
-  output <- data.frame(items = rep(NA, x$sets@Dim[1]), support = rep(NA, x$sets@Dim[1]))
-  for (i in 1:x$sets@Dim[1]) {
+  n <- x$sets@Dim[1]
+  output <- data.frame(items = rep(NA, n), support = rep(NA, n))
+  for (i in 1:n) {
     output[i, 1] <- x$sets@Dimnames[[1]][i]
     output[i, 2] <- x$support[i]
   }
   print(output)
 })
-#print(frequent_items)
 
-
-## Rules
+## Rules ## 
 # Idea here is to simply display the rules in a way that a human can read them easily: if lhs is purchased => rhs is frequently purchased, too (+support & confidence).
 
 # setClass()  #Placeholder to implement Rules properly as an S4 class later on
 
-# For now we create an S3 class from Rules and coerce it into an S4 class (Reactivate this block after building package)
-#class(Rules) <- "Rules"
-#setOldClass("Rules") # currently not working as Rules stays an S4 class object the method however works fine afterwards
-#isS4(Rules) 
-
 setMethod("print", signature(x = "Rules"), function(x) {
-  print(ExtractRules(Rules))
+  print(ExtractRules(Rules, maxNumConsequent = 1))
 })
-#print(Rules)
+
 
 
 
---
+++ examples/Methods.R
@@ -0,0 +1,39 @@
+##### This file contains examples for all methods #####
+#######################################################
+
+library(devtools)
+library(roxygen2)
+library(testthat)
+library(Rcpp)
+library(ProjectApriori)
+library(profvis)
+library(pryr)
+
+data("Groceries")
+TransactionMatrix <- makeTransactionMatrix(Groceries) #Sparse matrix of itemsets, where rownames == itemnames
+frequent_items <- FrequentItemsets(TransactionMatrix, 0.01)# Sparse matrix of frequent itemsets + supportvector containing support for each itemset
+Rules <- AssociationRules(Itemsets=TransactionMatrix, minsupport = 0.01, minconfidence = 0.5, arefrequent = FALSE) #Object of 'Rule' class (right now it's just 4 lists)
+print.rules <- ExtractRules(Rules) ##This should use generic print and output the rules nicely (Billo Version)
+
+class(TransactionMatrix)
+class(frequent_items)
+class(Rules)
+
+## check if we have methods listed for following classes: TransactionMatrix [currently ngTMatrix], FrequentItems [currently list], Rules [currently list]
+showMethods("print") 
+
+# For now we create an S3 class from frequent_items and coerce it into an S4 class (Reactivate this block after building package)
+class(frequent_items) <- "FrequentItems" 
+setOldClass("FrequentItems") 
+isS4(frequent_items)
+
+# For now we create an S3 class from Rules and coerce it into an S4 class (Reactivate this block after building package)
+class(Rules) <- "Rules"
+setOldClass("Rules") # currently not working as Rules stays an S4 class object the method however works fine afterwards
+isS4(Rules) 
+
+
+#Preview how the output looks currently
+print(TransactionMatrix)
+print(frequent_items)
+print(Rules)
