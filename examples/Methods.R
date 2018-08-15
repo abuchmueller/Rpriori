469b3b5813b3e29ce30a0d3a51d8183549c9dad5
anbu3000 <andreas.buchmueller@icloud.com>
2018-08-15 23:08:31 +0200

    added class constructor functions to examples

--- examples/Methods.R
+++ examples/Methods.R
@@ -13,7 +13,9 @@ data("Groceries")
 TransactionMatrix <- makeTransactionMatrix(Groceries) #Sparse matrix of itemsets, where rownames == itemnames
 frequent_items <- FrequentItemsets(TransactionMatrix, 0.01)# Sparse matrix of frequent itemsets + supportvector containing support for each itemset
 Rules <- AssociationRules(Itemsets=TransactionMatrix, minsupport = 0.01, minconfidence = 0.5, arefrequent = FALSE) #Object of 'Rule' class (right now it's just 4 lists)
-print.rules <- ExtractRules(Rules) ##This should use generic print and output the rules nicely (Billo Version)
+print.rules <- function(x) {
+  ExtractRules(x) ##This should use generic print and output the rules nicely (Billo Version)
+}
 
 class(TransactionMatrix)
 class(frequent_items)
@@ -37,3 +39,17 @@ isS4(Rules)
 print(TransactionMatrix)
 print(frequent_items)
 print(Rules)
+
+
+## Classes ##
+# this creates a TAMatrix object from a sparse Matrix from the makeTransactionMatrix function
+TSM <- new("TAMatrix", data  = TransactionMatrix, 
+            dim = TransactionMatrix@Dim, items = TransactionMatrix@Dimnames[[1]])
+
+#Frequent Itemsets
+fitems <- new("FIMatrix", data = frequent_items$sets, support = frequent_items$support)
+
+#Rules
+rulez <- new("Rules", lhs = Rules$lhs, rhs = Rules$rhs, support = Rules$support, confidence = Rules$confidence)
+
+
