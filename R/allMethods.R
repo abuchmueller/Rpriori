88c377a9bb706e6feffe2ed81174d2ee4fe00c61
anbu3000 <andreas.buchmueller@icloud.com>
2018-08-15 23:08:07 +0200

    renamed (now stores all methods)

--- R/allMethods.R
+++ R/allMethods.R
@@ -1,32 +1,36 @@
 #### This file stores printing methods for all classes (Itemsets, FrequentItems & Rules) ####
 #############################################################################################
 
+#' @include allClasses.R
+
 # This tells the generic print function what to print when the object is of class ngTMatrix
 # Printing method for TransactionsMatrix class (later)
 # For now it prints the names of the items in the TransactionMatrix
 
-setMethod("print", signature(x = "ngTMatrix"), function(x) {
+
+setMethod("print", signature(x = "TAMatrix"), function(x) {
   print(x@Dimnames[[1]])
 })
 
-
-# Printing method for transactions class
-setMethod("print", signature(x = "transactions"), function(x) {
-  print(x@itemInfo)
+setMethod("length", signature(x = "TAMatrix"), function(x) {
+  return(x@dim[1])
 })
+
+# setMethod("summary", signature(x = "TAMatrix"), function(x) {
+#   print(x@dim)
+# })
+
 
 ## FrequentItems ##
 # Idea here is to combine the elements of both lists, i.e., a frequent item and it's corressponding support and display them side by side column wise in a data frame
 
-# setClass() ## Placeholder to implement FrequentItems properly as an S4 class later on
-
 # Printing method for FrequentItems class
-setMethod("print", signature(x = "FrequentItems"), function(x) {
-  n <- x$sets@Dim[1]
+setMethod("print", signature(x = "FIMatrix"), function(x) {
+  n <- x@data@Dim[1]
   output <- data.frame(items = rep(NA, n), support = rep(NA, n))
   for (i in 1:n) {
-    output[i, 1] <- x$sets@Dimnames[[1]][i]
-    output[i, 2] <- x$support[i]
+    output[i, 1] <- x@data@Dimnames[[1]][i]
+    output[i, 2] <- x@support[i]
   }
   print(output)
 })
@@ -34,11 +38,10 @@ setMethod("print", signature(x = "FrequentItems"), function(x) {
 ## Rules ## 
 # Idea here is to simply display the rules in a way that a human can read them easily: if lhs is purchased => rhs is frequently purchased, too (+support & confidence).
 
-# setClass()  #Placeholder to implement Rules properly as an S4 class later on
-
 setMethod("print", signature(x = "Rules"), function(x) {
   print(ExtractRules(Rules, maxNumConsequent = 1))
 })
+
 
 
 
