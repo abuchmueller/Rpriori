2bd79802307d2efbffa1045c3e9415293383a66a
anbu3000 <andreas.buchmueller@icloud.com>
2018-08-15 23:06:34 +0200

    added classes

---
+++ R/allClasses.R
@@ -0,0 +1,58 @@
+#### This file stores all classes (Itemsets, FrequentItems & Rules) ####
+########################################################################
+
+#' An S4 class to represent an Itemset as a Sparse Matrix
+#' @name TAMatrix
+#' @rdname Transactionmatrix-class
+#' @slot data Transaction Matrix in sparse representation
+#' @exportClass TAMatrix
+setClass("TAMatrix",
+        representation(
+           data  = "ngTMatrix", 
+           dim  = "ANY",
+           items = "ANY"
+         ),
+       #lacks validity checking
+       prototype(dim = data.frame(), items=data.frame())
+        
+)
+
+setClass("TAMatrix2", 
+         slots = list(
+           data = "ngTMatrix", 
+           dim = "integer", 
+           items = "character")
+)
+
+
+#' An S4 class to represent an frequent Itemsets in a sparse Matrix
+#' @name FIMatrix
+#' @rdname Frequentitems-class
+#' @slot data Frequent Itemset Matrix in sparse representation
+#' @slot support Corresponding support
+#' @exportClass FIMatrix
+setClass("FIMatrix", 
+         representation(
+           data = "ngTMatrix",
+           support = "ANY"),
+         prototype(support = data.frame())
+)
+
+#' An S4 class to represent an frequent Itemsets in a sparse Matrix
+#' @name Rules
+#' @rdname Rules-class  
+#' @slot lhs Left-hand side 
+#' @slot rhs Right-hande side
+#' @slot support Support vector of Rules
+#' @slot confidence Confidence vector of Rules
+#' @exportClass Rules
+setClass("Rules",
+         representation(
+           lhs = "ngTMatrix",
+           rhs = "ngTMatrix",
+           support = "numeric",
+           confidence = "numeric"
+         )
+)
+
+
